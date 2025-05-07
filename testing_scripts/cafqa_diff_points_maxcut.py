1f5a05be4b1657a53afdb1a6d298b1e86ace1407
Dhanvi Bharadwaj <dhanvib@umich.edu>
2025-05-07 09:50:05 -0700

    fix: Add option for choosing which fitness values to run analysis on

--- testing_scripts/cafqa_diff_points_maxcut.py
+++ testing_scripts/cafqa_diff_points_maxcut.py
@@ -21,7 +21,6 @@ from clapton.circuit_manipulation import transform_to_allowed_gates,qiskit_to_st
 from testing_scripts.graphs_utils import generate_random_complete_graph,generate_k_regular_graph, build_max_cut_paulis, compute_optimal_max_cut
 from testing_scripts.qaoa_utils import QAOASolver,evaluate_energy
 from maxcut_processing import evaluate_maxcut
-print("Command-line arguments:", sys.argv)
 
 import multiprocess as mp
 import traceback
@@ -30,7 +29,7 @@ import numpy as np
 
 def run_qaoa_task_pool(args):
 
-    max_iters = 2000
+    max_iters = 4000
     task_id, maxcut_qaoa, initial_params, fitness_val = args
 
     # QAOA Optimization
@@ -38,7 +37,7 @@ def run_qaoa_task_pool(args):
 
     try:
         print(f"Starting task: {task_id} for val : {fitness_val}")
-        result, obj_values = maxcut_qaoa.run_qaoa(initial_params=cafqa_params, max_iters=max_iters, opt="COBYQA")
+        result, obj_values = maxcut_qaoa.run_qaoa(initial_params=cafqa_params, max_iters=max_iters, opt="SPSA")
         print(f"Finished task: {task_id}")
         return {
             "task_name": task_id,
@@ -57,25 +56,25 @@ def run_qaoa_task_pool(args):
         }
 
 # Function to execute in parallel using Pool
-def execute_qaoa_tasks(ma_qaoa_object, vanilla_qaoa_object, selected_cafqa_parameters, selected_fitness_values):
+def execute_qaoa_tasks(ma_qaoa_object, vanilla_qaoa_object, selected_cafqa_parameters, selected_spaced_fitness_vals):
 
     cafqa_args = [
         (f"task_{i}", ma_qaoa_object, params, fitness)
-        for i, (params, fitness) in enumerate(zip(selected_cafqa_parameters, selected_fitness_values))
+        for i, (params, fitness) in enumerate(zip(selected_cafqa_parameters, selected_spaced_fitness_vals))
     ]
 
     # Random Init. Params for MA-QAOA 
     random_angles = np.random.random(ma_qaoa_object.pcirc.num_parameters)
-    random_args = [("Random MA-QAOA",ma_qaoa_object,random_angles,selected_fitness_values)]
+    random_args = [("Random_MA-QAOA",ma_qaoa_object,random_angles,selected_spaced_fitness_vals)]
 
     # Prepare task for Vanilla QAOA
     random_vanilla_angles = np.random.random(vanilla_qaoa_object.circuit.num_parameters)
-    vanilla_args = [("Vanilla_task", vanilla_qaoa_object, random_vanilla_angles, selected_fitness_values)]
+    vanilla_args = [("Vanilla_task", vanilla_qaoa_object, random_vanilla_angles, selected_spaced_fitness_vals)]
 
     # Combine all tasks
     all_args = cafqa_args + random_args + vanilla_args 
 
-    with mp.Pool(processes=len(selected_cafqa_parameters)) as pool:
+    with mp.Pool(processes=len(all_args)) as pool:
         results_list = pool.map(run_qaoa_task_pool, all_args)
 
     results = {res["task_name"]: res for res in results_list}
@@ -111,7 +110,7 @@ def main():
     G = generate_random_complete_graph(num_vertices=n, weighted=True, seed=True)
 
     # Evaluate Optimal Maxcut
-    optimal_max_cut_val = compute_optimal_max_cut(G)
+    # optimal_max_cut_val = compute_optimal_max_cut(G)
 
     # Build the cost Hamiltonian
     max_cut_paulis = build_max_cut_paulis(G)
@@ -130,31 +129,35 @@ def main():
     print(f"Minimum Energy found with CAFQA initialization: {maxcut_qaoa.energy_best}")
 
     # Best Solutions
-    best_cafqa_fitness_values = maxcut_qaoa.best_cafqa_gen_fitness
-    best_cafqa_parameters = maxcut_qaoa.best_cafqa_gen_params
+    best_cafqa_fitness_values = maxcut_qaoa.best_cafqa_gen_fitness[::-1]
+    best_cafqa_parameters = maxcut_qaoa.best_cafqa_gen_params[::-1]
 
     unique_fitness_values = np.unique(best_cafqa_fitness_values)
 
-    selected_fitness_values = list(unique_fitness_values[::3][:5])  # Select 5 of the best spaced-out solutions.
-
-    selected_fitness_indices = [best_cafqa_fitness_values.index(value) for value in selected_fitness_values]
-    selected_cafqa_parameters = np.array(best_cafqa_parameters)[selected_fitness_indices]
-
+    # When choosing best out of unique energies
+    # selected_spaced_fitness_vals = list(unique_fitness_values[::3][:5]) #Select 5 of the best unique spaced-out solutions.
+    # selected_spaced_fitness_vals = list(unique_fitness_values[:5]) #Select 5 of the best unique solutions.
+    # selected_fitness_indices = [best_cafqa_fitness_values.index(value) for value in selected_spaced_fitness_vals]
+    # selected_cafqa_parameters = np.array(best_cafqa_parameters)[selected_fitness_indices]
+    
+    #When choosing just best 5 (can be same energies) 
+    selected_spaced_fitness_vals = list(best_cafqa_fitness_values)[:5] #Select 5 of the best solutions (can be repeated).
+    selected_cafqa_parameters = np.array(best_cafqa_parameters)[:5]
+    
     # Vanilla QAOA
     circuit = QAOAAnsatz(cost_operator=cost_hamiltonian, reps=reps)
     vanilla_maxcut = QAOASolver(cost_hamiltonian,circuit.decompose().decompose())
     vanilla_maxcut.vanilla = True
-    # vanilla_res,vanilla_obj_vals=vanilla_maxcut.run_qaoa(vanilla_random_vanilla_angles, max_iters=max_iters)
 
     start = timer()
-    results = execute_qaoa_tasks(maxcut_qaoa, vanilla_maxcut, selected_cafqa_parameters, selected_fitness_values)
+    results = execute_qaoa_tasks(maxcut_qaoa, vanilla_maxcut, selected_cafqa_parameters, selected_spaced_fitness_vals)
     end = timer()
     print(f"Total Time : {end - start}")
 
     # Save results
     output_dir = f"../np_data/CAFQA_Analysis/{n_qubits}_qbs"
     os.makedirs(output_dir, exist_ok=True)
-    np.save(os.path.join(output_dir, f"COBYQA_point_analysis_{seed}.npy"), results)
+    np.save(os.path.join(output_dir, f"SPSA_best_same_5_point_analysis_{seed}.npy"), results)
 
 # Entry point
 if __name__ == "__main__":
