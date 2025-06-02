03bfedadf8c9a119f2cee22dce3ff5a1b44cac4c
Dhanvi Bharadwaj <dhanvib@umich.edu>
2025-06-01 21:09:00 -0700

    refactor: add approximation ration computation in maxcut script

--- testing_scripts/cafqa_diff_points_maxcut.py
+++ testing_scripts/cafqa_diff_points_maxcut.py
@@ -18,7 +18,7 @@ from timeit import default_timer as timer
 sys.path.append("../")
 from clapton.clapton import claptonize
 from clapton.circuit_manipulation import transform_to_allowed_gates,qiskit_to_stim, modify_circuit, multi_angle_qaoa_circuit, generate_qiskit_param_map
-from testing_scripts.graphs_utils import generate_random_complete_graph,generate_k_regular_graph, build_max_cut_paulis, compute_optimal_max_cut
+import testing_scripts.graphs_utils as graph_utils
 from testing_scripts.qaoa_utils import QAOASolver
 from maxcut_processing import evaluate_maxcut
 
@@ -29,22 +29,29 @@ import numpy as np
 
 def run_qaoa_task_pool(args):
 
-    max_iters = 100
-    task_id, maxcut_qaoa, initial_params, fitness_val = args
+    max_iters = 10*1e+3
+    task_id, maxcut_qaoa, initial_params, fitness_val, graph = args
 
     # QAOA Optimization
     cafqa_params = [param * (np.pi / 2) for param in initial_params]
 
     try:
         print(f"Starting task: {task_id} for val : {fitness_val}")
-        result, obj_values = maxcut_qaoa.run_qaoa(initial_params=cafqa_params, max_iters=max_iters, opt="SPSA")
+        result, obj_values = maxcut_qaoa.run_qaoa(initial_params=cafqa_params, max_iters=max_iters, opt="COBYLA")
         print(f"Finished task: {task_id}")
+
+        # Approximation Ratio
+        distribution = graph_utils.get_final_distribution(maxcut_qaoa,result.x)
+        approx_ratio = graph_utils.calculate_approximation_ratio(distribution,graph)
+        print(approx_ratio)
+
         return {
             "task_name": task_id,
             "result": result,
             "fitness_val": fitness_val,
             "obj_values": obj_values,
             "final_energy": result.fun,
+            "approx_ratio": approx_ratio
         }
     
     except Exception as e:
@@ -56,20 +63,20 @@ def run_qaoa_task_pool(args):
         }
 
 # Function to execute in parallel using Pool
-def execute_qaoa_tasks(ma_qaoa_object, vanilla_qaoa_object, selected_cafqa_parameters, selected_spaced_fitness_vals):
+def execute_qaoa_tasks(ma_qaoa_object, vanilla_qaoa_object, selected_cafqa_parameters, selected_spaced_fitness_vals, graph):
 
     cafqa_args = [
-        (f"task_{i}", ma_qaoa_object, params, fitness)
+        (f"task_{i}", ma_qaoa_object, params, fitness , graph)
         for i, (params, fitness) in enumerate(zip(selected_cafqa_parameters, selected_spaced_fitness_vals))
     ]
 
     # Random Init. Params for MA-QAOA 
     random_angles = np.random.random(ma_qaoa_object.pcirc.num_parameters)
-    random_args = [("Random_MA-QAOA",ma_qaoa_object,random_angles,selected_spaced_fitness_vals)]
+    random_args = [("Random_MA-QAOA",ma_qaoa_object,random_angles,selected_spaced_fitness_vals, graph)]
 
     # Prepare task for Vanilla QAOA
     random_vanilla_angles = np.random.random(vanilla_qaoa_object.circuit.num_parameters)
-    vanilla_args = [("Vanilla_task", vanilla_qaoa_object, random_vanilla_angles, selected_spaced_fitness_vals)]
+    vanilla_args = [("Vanilla_task", vanilla_qaoa_object, random_vanilla_angles, selected_spaced_fitness_vals, graph)]
 
     # Combine all tasks
     all_args = cafqa_args + random_args + vanilla_args 
@@ -107,13 +114,10 @@ def main():
     k = 3  # for k-regular graphs
 
     # Generate the graph
-    G = generate_random_complete_graph(num_vertices=n, weighted=True, seed=True)
-
-    # Evaluate Optimal Maxcut
-    # optimal_max_cut_val = compute_optimal_max_cut(G)
+    G = graph_utils.generate_random_complete_graph(num_vertices=n, weighted=True, seed=seed)
 
     # Build the cost Hamiltonian
-    max_cut_paulis = build_max_cut_paulis(G)
+    max_cut_paulis = graph_utils.build_max_cut_paulis(G)
     cost_hamiltonian = SparsePauliOp.from_list(max_cut_paulis)
 
     # Create the QAOA circuit
@@ -134,16 +138,16 @@ def main():
 
     unique_fitness_values = np.unique(best_cafqa_fitness_values)
 
-    # # When choosing best out of unique energies
-    # selected_spaced_fitness_vals = list(unique_fitness_values[::3][:5]) #Select 5 of the best unique spaced-out solutions.
+    # When choosing best out of unique energies
+    selected_spaced_fitness_vals = list(unique_fitness_values[::3][:5]) #Select 5 of the best unique spaced-out solutions.
 
     # selected_spaced_fitness_vals = list(unique_fitness_values[:5]) #Select 5 of the best unique solutions.
-    # selected_fitness_indices = [best_cafqa_fitness_values.index(value) for value in selected_spaced_fitness_vals]
-    # selected_cafqa_parameters = np.array(best_cafqa_parameters)[selected_fitness_indices]
+    selected_fitness_indices = [best_cafqa_fitness_values.index(value) for value in selected_spaced_fitness_vals]
+    selected_cafqa_parameters = np.array(best_cafqa_parameters)[selected_fitness_indices]
     
-    #When choosing just best 5 (can be same energies) 
-    selected_spaced_fitness_vals = list(best_cafqa_fitness_values)[:5] #Select 5 of the best solutions (can be repeated).
-    selected_cafqa_parameters = np.array(best_cafqa_parameters)[:5]
+    # # #When choosing just best 5 (can be same energies) 
+    # selected_spaced_fitness_vals = list(best_cafqa_fitness_values)[:5] #Select 5 of the best solutions (can be repeated).
+    # selected_cafqa_parameters = np.array(best_cafqa_parameters)[:5]
     
     # Vanilla QAOA
     circuit = QAOAAnsatz(cost_operator=cost_hamiltonian, reps=reps)
@@ -151,14 +155,14 @@ def main():
     vanilla_maxcut.vanilla = True
 
     start = timer()
-    results = execute_qaoa_tasks(maxcut_qaoa, vanilla_maxcut, selected_cafqa_parameters, selected_spaced_fitness_vals)
+    results = execute_qaoa_tasks(maxcut_qaoa, vanilla_maxcut, selected_cafqa_parameters, selected_spaced_fitness_vals, G)
     end = timer()
     print(f"Total Time : {end - start}")
 
-    # # Save results
-    # output_dir = f"../np_data/CAFQA_Analysis/{n_qubits}_qbs"
-    # os.makedirs(output_dir, exist_ok=True)
-    # np.save(os.path.join(output_dir, f"SPSA_best_same_5_point_analysis_{seed}.npy"), results)
+    # Save results
+    output_dir = f"../np_data/CAFQA_Analysis/Approx_Ratio/Maxcut/{n_qubits}_qbs"
+    os.makedirs(output_dir, exist_ok=True)
+    np.save(os.path.join(output_dir, f"approx_ratio_Long__more_gens_COBYLA_best_spaced_5_point_analysis_{seed}.npy"), results)
 
 # Entry point
 if __name__ == "__main__":
